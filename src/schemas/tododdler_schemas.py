2c479833899811572579e58bb9399e45560064bb
MeNaemS <kb135764@gmail.com>
2025-01-18 21:07:35 +0300

    Fix

--- src/schemas/tododdler_schemas.py
+++ src/schemas/tododdler_schemas.py
@@ -17,5 +17,49 @@ class TododdlerRequest(BaseSchema):
 
 
 @dataclass
-class TododdlerCreateSpace(BaseSchema):
-    pass
+class BaseTododdler(BaseSchema):
+    id: 204
+    title: str
+    createdAt: str
+    updatedAt: str
+
+
+@dataclass
+class ReTododdler(BaseTododdler):
+    primary: bool
+
+
+@dataclass
+class TododdlerLinks(BaseSchema):
+    leftCardId: int
+    rightCardId: int
+    linkType: str
+    createdAt: str
+    updatedAt: str
+
+
+@dataclass
+class TododdlerCardSchema(BaseSchema):
+    board_id: int
+    id: int
+    title: str
+    description: str
+    creatorId: str
+    deadline: str
+    type: str
+    createdAt: str
+    updatedAt: str
+    links: list[TododdlerLinks]
+
+
+@dataclass
+class TododdlerAttachemtns(BaseSchema):
+    id: str
+    filename: str
+    fileType: str
+    url: str
+
+
+@dataclass
+class TododdlerCardAttachmentSchema(TododdlerCardSchema):
+    attachemnts: list[TododdlerAttachemtns]
